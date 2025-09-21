d1b9bea09a9b754b6334b51d689b2e586a6659b4
noba-w <noe.alba.freelance@gmail.com>
2025-09-21 19:30:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-def factorial(n):
-    resultado = 1
-    for i in range(1, n + 1):
-        resultado *= i
-    return resultado
 
-try:
-    numero = int(input("Introduce un número entero: "))
-    if numero < 0:
-        print("El factorial no está definido para números negativos.")
-    else:
-        print(f"El factorial de {numero} es {factorial(numero)}")
-except ValueError:
     print("Por favor, introduce un número entero válido.")
